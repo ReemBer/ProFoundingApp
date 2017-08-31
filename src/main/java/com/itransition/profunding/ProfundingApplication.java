28d495bc830af644741f4b7b63f59349573f519b
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-08-31 17:45:18 +0300

    Created simple project structure. Added spring modules.

---
+++ src/main/java/com/itransition/profunding/ProfundingApplication.java
@@ -0,0 +1,12 @@
+package com.itransition.profunding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfundingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfundingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/itransition/profunding/ProfundingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itransition.profunding;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProfundingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
