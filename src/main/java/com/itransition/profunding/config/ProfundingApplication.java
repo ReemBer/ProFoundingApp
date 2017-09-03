aa0511f97bbab92cfa70c791f74ca04b063da7fb
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-03 19:23:02 +0300

    Added basic packages structure. Activated profile "dev".

--- src/main/java/com/itransition/profunding/config/ProfundingApplication.java
+++ src/main/java/com/itransition/profunding/config/ProfundingApplication.java
@@ -1,4 +1,4 @@
-package com.itransition.profunding;
+package com.itransition.profunding.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
