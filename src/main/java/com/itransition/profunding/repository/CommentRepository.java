619c4d0c460b7dbf06c0756ea90828ab2e41f8e0
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-07 21:50:29 +0300

    Repositories are marked with an annotation @Repository

--- src/main/java/com/itransition/profunding/repository/CommentRepository.java
+++ src/main/java/com/itransition/profunding/repository/CommentRepository.java
@@ -2,11 +2,13 @@ package com.itransition.profunding.repository;
 
 import com.itransition.profunding.model.DB.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author v.tarasevich
  * @version 1.0
  * @since 07.09.2017 6:08
  */
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
