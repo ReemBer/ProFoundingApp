5db71856ab9dfaa39058a64e111ed9dc5da9e01c
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-08 18:31:50 +0300

    Added AuthUserTransformer class, what provide transformation from User entity to object of AuthUserDto class.

---
+++ src/main/java/com/itransition/profunding/service/transformer/AuthUserTransformer.java
@@ -0,0 +1,24 @@
+package com.itransition.profunding.service.transformer;
+
+import com.itransition.profunding.model.DB.User;
+import com.itransition.profunding.model.dto.AuthUserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author v.tarasevich
+ * @version 1.0
+ * @since 08.09.2017 16:29
+ */
+@Component
+@RequiredArgsConstructor
+public class AuthUserTransformer {
+
+    public AuthUserDto makeDto(final User user) {
+        AuthUserDto authUserDto = new AuthUserDto();
+        authUserDto.setId(user.getId());
+        authUserDto.setUsername(user.getUsername());
+        authUserDto.setRole(user.getUserRole().name());
+        return authUserDto;
+    }
+}
