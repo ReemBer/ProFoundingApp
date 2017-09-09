f2ab6395f717407692a7670d690beda92848a7a6
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-09 18:27:40 +0300

    Added http method "OPTIONS" to allowed methods. Web configuration are imported into SppringBootApplication.

--- src/main/java/com/itransition/profunding/ProfundingApplication.java
+++ src/main/java/com/itransition/profunding/ProfundingApplication.java
@@ -1,12 +1,13 @@
 package com.itransition.profunding;
 
+import com.itransition.profunding.controller.WebConfiguration;
 import com.itransition.profunding.security.SecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({SecurityConfiguration.class})
+@Import({SecurityConfiguration.class, WebConfiguration.class})
 public class ProfundingApplication {
 
 	public static void main(String[] args) {
