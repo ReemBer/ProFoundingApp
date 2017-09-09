0e624ebc09aa50847c59a365f9fe28bc3454b4b2
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-09 19:10:47 +0300

    Property source files added to root config class

--- src/main/java/com/itransition/profunding/ProfundingApplication.java
+++ src/main/java/com/itransition/profunding/ProfundingApplication.java
@@ -5,9 +5,11 @@ import com.itransition.profunding.security.SecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @Import({SecurityConfiguration.class, WebConfiguration.class})
+@PropertySource({"classpath:custom.properties", "classpath:application.properties"})
 public class ProfundingApplication {
 
 	public static void main(String[] args) {
