5e94e4725371c0f2cbce88dcfbfda05b3107cd71
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-11 01:46:40 +0300

    Created Entity, what store unconfirmed registration data

---
+++ src/main/java/com/itransition/profunding/model/db/RegistrationData.java
@@ -0,0 +1,40 @@
+package com.itransition.profunding.model.db;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+/**
+ * @author v.tarasevich
+ * @version 1.0
+ * @since 10.09.2017 17:00
+ */
+@Entity
+@Table(name = "unconfirmed_registration_data")
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "expiration_time")
+    private Long expirationTime;
+
+    @Column(name = "registration_hash")
+    private String registrationHash;
+}
