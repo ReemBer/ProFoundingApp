6488ed09ad4f1e5abeec4e7153a0f5b5b6eba955
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-11 01:53:47 +0300

    Added Dto classes to sending registration data from client, and sending response about registration status.

---
+++ src/main/java/com/itransition/profunding/model/dto/RegistrationResponseStatus.java
@@ -0,0 +1,13 @@
+package com.itransition.profunding.model.dto;
+
+/**
+ * @author v.tarasevich
+ * @version 1.0
+ * @since 10.09.2017 16:23
+ */
+public enum RegistrationResponseStatus {
+    OK,
+    ERROR,
+    SUCH_USERNAME_EXIST,
+    SUCH_MAIL_EXIST,
+}
