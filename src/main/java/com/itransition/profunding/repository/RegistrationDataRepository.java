b45a047f9271c77aa96d95d5c0c53e1c4037359a
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-11 01:57:57 +0300

    Added registration data repository

---
+++ src/main/java/com/itransition/profunding/repository/RegistrationDataRepository.java
@@ -0,0 +1,22 @@
+package com.itransition.profunding.repository;
+
+import com.itransition.profunding.model.db.RegistrationData;
+import com.itransition.profunding.model.dto.RegistrationRequestDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author v.tarasevich
+ * @version 1.0
+ * @since 10.09.2017 21:35
+ */
+@Repository
+public interface RegistrationDataRepository extends JpaRepository<RegistrationData, Long> {
+
+    void deleteByExpirationTimeLessThan(Long currentTime);
+
+
+    RegistrationData findByUsername(String username);
+    RegistrationData findByEmail(String email);
+}
