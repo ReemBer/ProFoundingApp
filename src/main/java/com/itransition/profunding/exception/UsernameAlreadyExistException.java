c7690e056a52d9ade9b322a848b395931baf4aa9
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-11 22:18:28 +0300

    added custom exception for checking for existing users.

---
+++ src/main/java/com/itransition/profunding/exception/UsernameAlreadyExistException.java
@@ -0,0 +1,13 @@
+package com.itransition.profunding.exception;
+
+/**
+ * @author v.tarasevich
+ * @version 1.0
+ * @since 10.09.2017 16:54
+ */
+public class UsernameAlreadyExistException extends RuntimeException {
+
+    public UsernameAlreadyExistException() {
+        super("User with such username already exist.");
+    }
+}
