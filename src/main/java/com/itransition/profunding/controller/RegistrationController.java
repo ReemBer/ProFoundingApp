958e189b97d40645bce794333046a7b545907838
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-11 22:40:26 +0300

    added Registration controller

---
+++ src/main/java/com/itransition/profunding/controller/RegistrationController.java
@@ -0,0 +1,35 @@
+package com.itransition.profunding.controller;
+
+import com.itransition.profunding.model.dto.ConfirmRegistrationResponseStatus;
+import com.itransition.profunding.model.dto.RegistrationRequestDto;
+import com.itransition.profunding.model.dto.RegistrationResponseDto;
+import com.itransition.profunding.service.RegistrationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author v.tarasevich
+ * @version 1.0
+ * @since 09.09.2017 17:48
+ */
+@Controller
+@CrossOrigin
+@RequestMapping(value = "/registration")
+@RequiredArgsConstructor
+public class RegistrationController {
+
+
+    private final RegistrationService registrationService;
+
+    @PostMapping
+    public @ResponseBody RegistrationResponseDto register(@RequestBody RegistrationRequestDto registrationRequestDto){
+        return registrationService.register(registrationRequestDto);
+    }
+
+    @GetMapping(value = "/{registrationHash}")
+    public String confirm(@PathVariable String registrationHash) {
+        ConfirmRegistrationResponseStatus response = registrationService.confirm(registrationHash);
+        return "redirect:http://localhost:8081/login?status=" + response.name();
+    }
+}
