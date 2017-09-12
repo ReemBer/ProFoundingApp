b504928d3323a8ada1133386b252c66a7ccc99f0
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-12 22:59:07 +0300

    added encrypting password in RegistrationUserTransformer

--- src/main/java/com/itransition/profunding/service/transformer/RegistrationUserTransformer.java
+++ src/main/java/com/itransition/profunding/service/transformer/RegistrationUserTransformer.java
@@ -4,6 +4,8 @@ import com.itransition.profunding.model.db.RegistrationData;
 import com.itransition.profunding.model.db.User;
 import com.itransition.profunding.model.db.UserRole;
 import com.itransition.profunding.service.Transformer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,7 +14,11 @@ import org.springframework.stereotype.Service;
  * @since 11.09.2017 3:19
  */
 @Service
+@RequiredArgsConstructor
 public class RegistrationUserTransformer implements Transformer<User, RegistrationData> {
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public RegistrationData makeDto(User user) {
         throw new UnsupportedOperationException();
@@ -22,7 +28,7 @@ public class RegistrationUserTransformer implements Transformer<User, Registrati
     public User makeEntity(RegistrationData registrationData) {
         User user = new User();
         user.setUsername(registrationData.getUsername());
-        user.setPassword(registrationData.getPassword());
+        user.setPassword(passwordEncoder.encode(registrationData.getPassword()));
         user.setEmail(registrationData.getEmail());
         user.setUserRole(UserRole.ROLE_NO_PROOFED_USER);
         return user;
