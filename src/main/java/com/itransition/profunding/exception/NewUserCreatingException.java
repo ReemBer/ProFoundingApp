e4a9f994b9bb373e1c70655efd156a3b8abbaf93
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-14 03:43:07 +0300

    added Exception handling for RegistrationController

---
+++ src/main/java/com/itransition/profunding/exception/NewUserCreatingException.java
@@ -0,0 +1,28 @@
+package com.itransition.profunding.exception;
+
+/**
+ * @author v.tarasevich
+ * @version 1.0
+ * @since 14.09.2017 2:42
+ */
+public class NewUserCreatingException extends RuntimeException {
+    public NewUserCreatingException() {
+        super();
+    }
+
+    public NewUserCreatingException(String message) {
+        super(message);
+    }
+
+    public NewUserCreatingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NewUserCreatingException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NewUserCreatingException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ src/main/java/com/itransition/profunding/exception/RegistrationDataNotFoundException.java
@@ -0,0 +1,28 @@
+package com.itransition.profunding.exception;
+
+/**
+ * @author v.tarasevich
+ * @version 1.0
+ * @since 14.09.2017 2:40
+ */
+public class RegistrationDataNotFoundException extends RuntimeException {
+    public RegistrationDataNotFoundException() {
+        super();
+    }
+
+    public RegistrationDataNotFoundException(String message) {
+        super(message);
+    }
+
+    public RegistrationDataNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RegistrationDataNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    protected RegistrationDataNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
