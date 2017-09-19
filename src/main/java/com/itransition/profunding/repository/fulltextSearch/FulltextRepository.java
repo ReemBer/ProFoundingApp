d89e0a6582624598743bfedefc939f826d262804
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-19 05:06:23 +0300

    fixed field name in the FulltextRepository

--- src/main/java/com/itransition/profunding/repository/fulltextSearch/FulltextRepository.java
+++ src/main/java/com/itransition/profunding/repository/fulltextSearch/FulltextRepository.java
@@ -48,7 +48,7 @@ public class FulltextRepository {
     private org.apache.lucene.search.Query createQuery(QueryBuilder queryBuilder, String searchQuery) {
         return queryBuilder.keyword().onFields("title",
                                                         "description",
-                                                        "finansalGoals.title",
+                                                        "financialGoals.title",
                                                         "tags.tagName")
                 .matching(searchQuery).createQuery();
     }
