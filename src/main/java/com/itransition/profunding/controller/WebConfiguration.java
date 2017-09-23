e0e4f992c6f81dc33e35608b0c814d86663385d5
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-23 21:13:30 +0300

    Changed expiration time for auth token

--- src/main/java/com/itransition/profunding/controller/WebConfiguration.java
+++ src/main/java/com/itransition/profunding/controller/WebConfiguration.java
@@ -1,8 +1,12 @@
 package com.itransition.profunding.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.annotation.Resource;
 
 /**
  * @author v.tarasevich
@@ -12,14 +16,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
-    private static final int MAX_AGE = 3600;
+    @Resource
+    private Environment environment;
+
+    private static final String PROP_MAX_AGE = "security.authentication.token.expiration_time";
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
+        String a = environment.getProperty(PROP_MAX_AGE);
+        long b = Long.parseLong(a);
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedHeaders("*")
                 .allowedMethods("PUT", "DELETE", "GET", "POST", "OPTIONS")
-                .allowCredentials(false).maxAge(MAX_AGE);
+                .allowCredentials(false)
+                .maxAge(Long.parseLong(environment.getProperty(PROP_MAX_AGE)));
     }
 }
