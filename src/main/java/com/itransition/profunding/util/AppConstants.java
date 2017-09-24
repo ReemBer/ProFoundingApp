b54407814ed488415802973257c7a1c2c2f088c2
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-24 05:52:24 +0300

    The page size parameter moved to ApplicationConstants class

---
+++ src/main/java/com/itransition/profunding/util/AppConstants.java
@@ -0,0 +1,11 @@
+package com.itransition.profunding.util;
+
+/**
+ * @author v.tarasevich
+ * @version 1.0
+ * @since 21.09.2017 3:20
+ */
+public class AppConstants {
+
+    public static final int PAGE_SIZE = 12;
+}
