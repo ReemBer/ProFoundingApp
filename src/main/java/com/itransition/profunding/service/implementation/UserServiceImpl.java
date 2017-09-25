3cb1f958bfc92de710aa771a84c802e2037551e9
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-25 06:09:22 +0300

    added checking in user update method

--- src/main/java/com/itransition/profunding/service/implementation/UserServiceImpl.java
+++ src/main/java/com/itransition/profunding/service/implementation/UserServiceImpl.java
@@ -4,9 +4,13 @@ import com.itransition.profunding.model.db.User;
 import com.itransition.profunding.model.dto.ProfileDto;
 import com.itransition.profunding.model.dto.UserImageOnlyDto;
 import com.itransition.profunding.repository.UserRepository;
+import com.itransition.profunding.security.SecurityHelper;
+import com.itransition.profunding.security.model.JwtUserDetails;
 import com.itransition.profunding.service.UserService;
 import com.itransition.profunding.service.transformer.ProfileTransformer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -31,6 +35,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Boolean updateUser(UserImageOnlyDto userImageOnlyDto) {
+        checkAccess(userImageOnlyDto);
         this.userRepository.updateUserById(userImageOnlyDto.getImage(), userImageOnlyDto.getId());
         User user = userRepository.findById(userImageOnlyDto.getId());
         if (user == null)
@@ -38,5 +43,14 @@ public class UserServiceImpl implements UserService {
         else if (user.getImage().equals(userImageOnlyDto.getImage()))
             return true;
         return false;
+    }
+
+    private void checkAccess(UserImageOnlyDto userImageOnlyDto) {
+        JwtUserDetails userDetails = (JwtUserDetails) SecurityHelper.getAuthenticationWithCheck().getPrincipal();
+        boolean accessed = userDetails.getId().equals(userImageOnlyDto.getId())
+                || userDetails.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        if (!accessed) {
+            throw new AccessDeniedException("You have not authorities for this operation.");
+        }
     }
 }
